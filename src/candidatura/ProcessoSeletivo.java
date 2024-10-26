2ab6f583c48d731b88a513e8275676f3913973fe
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-10-26 19:03:43 -0300

    Melhorias implementadas

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,2 +1,46 @@
+package candidatura;
+
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
+
+    public static void main(String[] args) {
+        selecaoCandidatos();
+    }
+    static void selecaoCandidatos(){
+
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE"};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("Candidato " + candidato + " Solicitou este valor de sálario " + salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O candidato " + candidato + " Foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+        if (salarioBase > salarioPretendido){
+            System.out.println("Ligar para o candidato");
+        } else if(salarioBase == salarioPretendido){
+            System.out.println("Ligar para o candidato com contra proposta");
+        } else {
+            System.out.println("Aguardando o resultado dos demais candidatos");
+        }
+    }
 }
+
+
